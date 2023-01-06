eadcc5a7382012c3edfab00de3a15659caf9afe7
Brett Amdur <brett.amdur@gmail.com>
2023-01-06 15:13:49 -0500

    Update test2.r

--- senateElections/test2.r
+++ senateElections/test2.r
@@ -6,8 +6,13 @@ help(package = "ggplot2")
 mySum <- 3 + 4
 print(mySum)
 
-mydf <- data.frame("Cat" = c("A", "B", "C"), Values = c(12, 10, 15))
+mydf <- data.frame("Cat" = c("A", "B", "C"), 
+    Values = c(12, 10, 15))
 
 my_graph <- ggplot(data = mydf, aes(x = Cat, y = Values)) +
     geom_bar(stat = "identity", color = "#000000", fill = "blue") +
     ggtitle("My Test Bar Graph")
+
+my_data <- ToothGrowth
+
+res.ftest <- var.test(len~supp, data = my_data)
